e4dc514e656d8f6efb3fd979fb41c69a1797cf56
Valérie Lavigne <valelavi@gmail.com>
2016-04-13 12:05:16 -0400

    Cloropleth!

--- Map/js/main.js
+++ Map/js/main.js
@@ -1,7 +1,8 @@
 
 // Will be used to the save the loaded JSON and CSV data
 var mapData = [];
-var iedData = []
+var iedData = [];
+var regionData = [];
 
 // Variables for the visualization instances
 var mapVis, timelineVis;
@@ -29,6 +30,32 @@ queue()
 
 		// Copy topo json data
 		mapData = mapTopoJson;
+		console.log(mapData);
+
+		console.log(iedData);
+		// Organize region data for region coloring
+		var idMap = {};
+		iedData.forEach(function(d) {
+			var region = d.REGION.trim(); // Remove whitespaces in some names
+
+			var regionId = convertRegion2Id(region);
+
+			if (idMap[regionId] == null) {
+				var regionObj = {};
+				regionObj.ID = regionId;
+				regionObj.IEDevents = 1;
+				regionObj.KIA = d.KIA;
+				regionObj.WIA = d.WIA;
+				idMap[regionId] = regionData.length;
+				regionData.push(regionObj);
+			}
+			else {
+				regionData[idMap[regionId]].IEDevents += 1;
+				regionData[idMap[regionId]].KIA += d.KIA;
+				regionData[idMap[regionId]].WIA += d.WIA;
+			}
+		});
+		console.log(regionData);
 
 		// Create the visualizations
 		createVis();
@@ -39,7 +66,7 @@ queue()
 function createVis() {
 
 	// Instantiate visualization objects here
-	mapVis = new Map("mapVis", iedData, mapData);
+	mapVis = new Map("mapVis", iedData, mapData, regionData);
 	timelineVis = new Timeline("timelineVis", iedData);
 
 }
@@ -52,4 +79,111 @@ function brushed() {
 
 	// Update map
 	mapVis.wrangleData();
+}
+
+
+function regionColorSelect() {
+
+	var selectBox = document.getElementById("regionColorSelect");
+	if (mapVis.dataType != selectBox.options[selectBox.selectedIndex].value) {
+		mapVis.dataType = selectBox.options[selectBox.selectedIndex].value;
+		mapVis.dataLabel = selectBox.options[selectBox.selectedIndex].text;
+		mapVis.updateVis();
+	}
+}
+
+
+function convertRegion2Id(regionName) {
+
+	var regionId = "";
+	//Big switch to get region id
+	switch(regionName) {
+		case "CRIMEA": // 0
+			regionId = "crimea";
+			break;
+		case "MIKOLAYIV": // 1
+			regionId = "mk";
+			break;
+		case "MIKOLAYV": // 1
+			regionId = "mk";
+			break;
+		case "CHERNIHIV": //2
+			regionId = "cn";
+			break;
+		case "RIVNE": // 3
+			regionId = "rv";
+			break;
+		case "CHERNIVTSI": // 4
+			regionId = "cv";
+			break;
+		case "IVANO-FRANKIVSK": //5
+			regionId = "if";
+			break;
+		// No data for 6 km
+		case "LVIV": // 7
+			regionId = "lviv";
+			break;
+		case "TERNOPOL": // 8
+			regionId = "te";
+			break;
+		case "ZAKARPATS'KA": // 9
+			regionId = "uz";
+			break;
+		case "VOLYN": // 10
+			regionId = "volyn";
+			break;
+		case "CHERKASY": // 11
+			regionId = "ck";
+			break;
+		// No data for 12 kr
+		case "KIEV": // 13
+			regionId = "kiev";
+			break;
+		case "�KIEV": // 13
+			regionId = "kiev";
+			break;
+		case "ODESSA": // 14
+			regionId = "od";
+			break;
+		case "VINNYTSIA": // 15
+			regionId = "vn";
+			break;
+		case "ZHITOMYR": // 16
+			regionId = "zt";
+			break;
+		case "ZHYTOMYR": // 16
+			regionId = "zt";
+			break;
+		case "SUMY": // 17
+			regionId = "sm";
+			break;
+		case "DNIPROPETROVSK": // 18
+			regionId = "dp";
+			break;
+		case "DONETSK": // 19
+			regionId = "dn";
+			break;
+		case "KHARKIV": // 20
+			regionId = "kh";
+			break;
+		case "LUHANKS": // 21
+			regionId = "lg";
+			break;
+		case "LUHANSK": // 21
+			regionId = "lg";
+			break;
+		case "POLTAVA": // 22
+			regionId = "pl";
+			break;
+		case "ZAPORIZHYE": // 23
+			regionId = "zp";
+			break;
+		case "KHERSON": // 24
+			regionId = "ks";
+			break;
+		default:
+		//
+	}
+
+	return regionId;
 }
--- Map/js/map.js
+++ Map/js/map.js
@@ -3,14 +3,20 @@
  * @param _parentElement 	-- the HTML element in which to draw the visualization
  * @param _iedData			-- the ied data
  * @param _mapData			-- the map data
+ * @param _regionData			-- the map regions data
  */
 
-Map = function(_parentElement, _iedData, _mapData){
+Map = function(_parentElement, _iedData, _mapData, _regionData){
     this.parentElement = _parentElement;
     this.iedData = _iedData;
     this.mapData = _mapData;
+    this.regionData = _regionData;
     this.displayData = []; // see data wrangling
     this.filter = [];
+
+    // For region color
+    this.dataType = "IEDevents";
+    this.dataLabel = "Nb IED Incidents";
 
     this.initVis();
 }
@@ -24,7 +30,7 @@ Map.prototype.initVis = function(){
 
     vis.margin = {top: 0, right: 0, bottom: 0, left: 0};
 
-    vis.width = 800 - vis.margin.left - vis.margin.right,
+    vis.width = 900 - vis.margin.left - vis.margin.right,
         vis.height = 600 - vis.margin.top - vis.margin.bottom;
 
     // SVG drawing area
@@ -34,8 +40,59 @@ Map.prototype.initVis = function(){
         .append("g")
         .attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top + ")");
 
+    // Set type and color scales
+    vis.colors = d3.scale.quantize().domain([0,1]).range(colorbrewer.Blues[7]);
+    vis.typeScale = d3.scale.linear().range([0,1]);
+
+
+    // Legend data
+    var legendColors = vis.colors.range();
+    var legendValues = [0];
+    vis.colors.range().forEach(function(d,i) {
+        legendValues.push(Math.round(vis.typeScale.invert(vis.colors.invertExtent(d)[1])));
+    });
+    legendValues.push("No data");
+    legendColors.push("lightgrey");
+    var side = 20;
+
+    // Create legend
+    var legend = vis.svg.append("g")
+        .attr("transform", "translate(150, 390)")
+        .attr("id", "legend");
+
+    // Legend title
+    legend.append("text")
+        .attr("text-anchor","end")
+        .attr("id","legendTitle")
+        .attr("transform", "translate(30,0)");
+
+    // Add color squares
+    legend.append("g").selectAll("rect")
+        .data(legendColors)
+        .enter()
+        .append("rect")
+        .attr("class", "legendColor")
+        .attr("fill", function (d) {return d;})
+        .attr("height", side)
+        .attr("width", side)
+        .attr("transform", function(d, i) {
+            return "translate(2," + (i*(side+3) + 20) +")";
+        });
+
+    // Add values
+    legend.append("g").selectAll("text")
+        .data(legendValues)
+        .enter()
+        .append("text")
+        .attr("class", "legendValue")
+        .attr("text-anchor","end")
+        .attr("transform", function(d, i) {
+            if (i==8) return "translate(0," + (i*(side+3)+15) +")";
+            return "translate(0," + (i*(side+3) + 20) +")";
+        });
+
     // Create projection
-    var projection = d3.geo.mercator().scale([2200]).center([33.35, 49.5]);
+    var projection = d3.geo.mercator().scale([2650]).center([32, 49.4]);
     vis.proj = projection;
 
     // Create D3 geo path
@@ -44,7 +101,6 @@ Map.prototype.initVis = function(){
     // Convert TopoJSON to GeoJSON
     var countries = topojson.feature(mapData, mapData.objects.countries).features;
     var regions = topojson.feature(mapData, mapData.objects.regions).features;
-    vis.regionData = regions;
 
     // Map TopoJSON data to the screen
     // Regions
@@ -53,7 +109,7 @@ Map.prototype.initVis = function(){
         .data(regions)
         .enter().append("path")
         .attr("class", "country")
-        .attr("id", function(d) { return d.properties.name;})
+        .attr("id", function(d) { return d.id;})
         .attr("d", path)
         .style("stroke", "grey")
         .style("stroke-width", 1)
@@ -71,9 +127,8 @@ Map.prototype.wrangleData = function() {
 
     var vis = this; // read about the this
 
-    // Filter with timeline later
+    // Filter with timeline
     vis.displayData = vis.iedData;
-
     if (vis.filter.length > 0) {
         vis.displayData = vis.iedData.filter(function (d) {
             var first = new Date(d.DATE) >= vis.filter[0];
@@ -90,6 +145,37 @@ Map.prototype.wrangleData = function() {
 Map.prototype.updateVis = function() {
 
     var vis = this; // read about the this
+
+    // Update scale
+    var maxValue = d3.max(regionData, function(d) {return d[vis.dataType];});
+    vis.typeScale.domain([0, maxValue]);
+
+    // Colors
+    vis.svg.selectAll("path")
+        .style("fill",function(d) {
+            // Color
+            var data;
+            regionData.forEach(function(r) {
+                if (r.ID == d.id) data = r;
+            });
+            if (data) {
+                var value = data[vis.dataType];
+                if (isNaN(value)) return "lightgrey";
+                return vis.colors(vis.typeScale(value));
+            }
+            else return "lightgrey";
+        });
+
+    // Update legend
+    vis.svg.select("#legendTitle").text(vis.dataLabel);
+    var legendValues = [0];
+    vis.colors.range().forEach(function(d,i) {
+        legendValues.push(Math.round(vis.typeScale.invert(vis.colors.invertExtent(d)[1])));
+    });
+    legendValues[legendValues.length-1] = "No data";
+    var entries = vis.svg.selectAll(".legendValue")
+        .data(legendValues)
+        .text(function (d) {return d;});
 
     // Add event circles
     var circ = vis.circlesG.selectAll("circle")
